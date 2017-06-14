08cbb342f2c23f04d6d4c77033b1f7293aa1e6b1
Mathew Topper <damm_horse@yahoo.co.uk>
2017-06-14 09:38:45 +0100

    Ensure target directory exists when writing YAML file.

--- tests/test_config_map.py
+++ tests/test_config_map.py
@@ -268,3 +268,24 @@ def test_write_yaml(tmpdir, directory):
     assert os.path.basename(str(locd.listdir()[0])) == "logging.yaml"
 
 
+def test_write_yaml_nodir(tmpdir, directory):
+
+    '''Test if the configuration file is correctly copied when directory is
+    missing.'''
+
+    # Make a local directory
+    locd = tmpdir.mkdir("config")
+    nodir = os.path.join(str(locd), "nodir")
+
+    # Create Logger object and change path to user data dir to tmp
+    configdir = Directory(nodir)
+    dirmap = DirectoryMap(configdir, directory)
+
+    yaml_reader = ReadYAML(dirmap, "logging.yaml")
+
+    test_list = ["curly", "larry", "moe"]
+    yaml_reader.write(test_list)
+
+    testdir = locd.join("nodir")
+    
+    assert os.path.basename(str(testdir.listdir()[0])) == "logging.yaml"
