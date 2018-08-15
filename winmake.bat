a0e31345574e3b0729b0d40cf929c688054ad4d3
Mathew Topper <damm_horse@yahoo.co.uk>
2018-08-15 17:25:19 +0100

    Add option to winmake.bat to run pylint for the module, tests and setup.py

--- winmake.bat
+++ winmake.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 SET curdir=%cd%
 
+
 IF "%1" == "install" CALL :Install
 IF "%1" == "test" CALL :Test
+IF "%1" == "pylint" CALL :Pylint
 
 :Return
 CD %curdir%
@@ -24,3 +26,12 @@ CD %~dp0 && (
     CALL python setup.py test -a "-rsx tests"
 ) || ( GOTO Return )
 EXIT /B 0
+
+:: Function to run pylint
+:Pylint
+CD %~dp0
+setlocal enableDelayedExpansion
+SET result=
+FOR /f "tokens=*" %%a in ('dir tests\*.py /B') DO SET result=!result!tests\%%a 
+CALL pylint setup.py polite %result%
+GOTO Return
